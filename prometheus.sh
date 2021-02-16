3f0f173d3534f62863fd3148c25d194d1bc9c993
Kunal07 <50284603+KunalNK@users.noreply.github.com>
2021-02-17 00:16:11 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -2,5 +2,5 @@
 sudo yum install wget -y
 wget https://github.com/prometheus/prometheus/releases/download/v2.25.0-rc.0/prometheus-2.25.0-rc.0.linux-amd64.tar.gz
 tar xvfz prometheus-*.tar.gz
-cd prometheus
+cd prometheus-2.25.0-rc.0.linux-amd64
 ./prometheus --config.file=prometheus.yml

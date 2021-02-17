6dcbbbf40d22a88442bbc5a52b7e75ac2de8fd63
Kunal07 <50284603+KunalNK@users.noreply.github.com>
2021-02-17 20:25:50 +0530

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,5 @@
+wget https://dl.grafana.com/oss/release/grafana-7.4.1-1.x86_64.rpm
+sudo yum install grafana-7.4.1-1.x86_64.rpm
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
